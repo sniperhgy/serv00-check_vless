1d01a3deac9a7f4ba2c99dbfde316936de876f9d
sniperhgy <sniperhgy@gmail.com>
2024-08-17 20:59:52 +0800

    Update run.py modify command to "sh ~/check_vless.sh"

--- run.py
+++ run.py
@@ -5,8 +5,6 @@ import json
 from datetime import datetime, timezone, timedelta
 
 def ssh_multiple_connections(hosts_info, command):
-    users = []
-    hostnames = []
     for host_info in hosts_info:
         hostname = host_info['hostname']
         username = host_info['username']
@@ -16,28 +14,30 @@ def ssh_multiple_connections(hosts_info, command):
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(hostname=hostname, port=22, username=username, password=password)
             stdin, stdout, stderr = ssh.exec_command(command)
-            user = stdout.read().decode().strip()
-            users.append(user)
-            hostnames.append(hostname)
+            user_stdout = stdout.read().decode().strip()
             ssh.close()
+            print('连接成功')
+            print(user_stdout)
         except Exception as e:
             print(f"用户：{username}，连接 {hostname} 时出错: {str(e)}")
-    return users, hostnames
+    return user_stdout
 
 ssh_info_str = os.getenv('SSH_INFO', '[]')
 hosts_info = json.loads(ssh_info_str)
 
-command = 'whoami'
-user_list, hostname_list = ssh_multiple_connections(hosts_info, command)
-user_num = len(user_list)
-content = "SSH服务器登录信息：\n"
-for user, hostname in zip(user_list, hostname_list):
-    content += f"用户名：{user}，服务器：{hostname}\n"
-beijing_timezone = timezone(timedelta(hours=8))
-time = datetime.now(beijing_timezone).strftime('%Y-%m-%d %H:%M:%S')
-menu = requests.get('https://api.zzzwb.com/v1?get=tg').json()
-loginip = requests.get('https://api.ipify.org?format=json').json()['ip']
-content += f"本次登录用户共： {user_num} 个\n登录时间：{time}\n登录IP：{loginip}"
+command = 'sh ~/check_vless.sh'
+user_list = ssh_multiple_connections(hosts_info, command)
+
+
+# user_num = len(user_list)
+# content = "SSH服务器登录信息：\n"
+# for user, hostname in zip(user_list, hostname_list):
+#     content += f"用户名：{user}，服务器：{hostname}\n"
+# beijing_timezone = timezone(timedelta(hours=8))
+# time = datetime.now(beijing_timezone).strftime('%Y-%m-%d %H:%M:%S')
+# menu = requests.get('https://api.zzzwb.com/v1?get=tg').json()
+# loginip = requests.get('https://api.ipify.org?format=json').json()['ip']
+# content += f"本次登录用户共： {user_num} 个\n登录时间：{time}\n登录IP：{loginip}"
 
 push = os.getenv('PUSH')
 
@@ -76,9 +76,9 @@ def telegram_push(message):
     if response.status_code != 200:
         print(f"发送消息到Telegram失败: {response.text}")
 
-if push == "mail":
-    mail_push('https://zzzwb.us.kg/test')
-elif push == "telegram":
-    telegram_push(content)
-else:
-    print("推送失败，推送参数设置错误")
+# if push == "mail":
+#    mail_push('https://zzzwb.us.kg/test')
+# elif push == "telegram":
+#    telegram_push(content)
+# else:
+#    print("推送失败，推送参数设置错误")
